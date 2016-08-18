1c2290d7a9a9c4b5835bee022ecff1c08559e139
Jong-Shian Wu <js@jong.sh>
2016-08-18 15:46:21 +0800

    -

---
+++ core.py
@@ -0,0 +1,14 @@
+def generate_enrollment_request():
+    pass
+
+
+def process_enrollment_response():
+    pass
+
+
+def generate_idassertion_request():
+    pass
+
+
+def process_idassertion_response():
+    pass
