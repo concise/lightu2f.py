f77a0aab6e41a9bddf21c225300943e1905159f5
Jong-Shian Wu <js@jong.sh>
2016-08-18 16:41:52 +0800

    -

--- newlib.py
+++ newlib.py
@@ -1,3 +1,29 @@
+__all__ = [
+    'generate_enrollment_request',
+    'process_enrollment_response',
+    'generate_idassertion_request',
+    'process_idassertion_response',
+]
+
+
+def generate_enrollment_request():
+    pass
+
+
+def process_enrollment_response():
+    pass
+
+
+def generate_idassertion_request():
+    pass
+
+
+def process_idassertion_response():
+    pass
+
+
+
+
 ##############################################################################
 #                                                                            #
 #   ECDSA signature with SHA-256 verification routines on secp256r1 curve    #
